7d0fa122f89ec4ac449ffbac2d958d93acffd37e
Dana Fulmer <DanaRFulmer@yahoo.com>
2024-06-19 16:02:46 -0600

    added error handling to get savings and savings output.

--- customer_banking.py
+++ customer_banking.py
@@ -30,18 +30,20 @@ def main():
     It displays the interest earned on the savings and CD accounts and updates the balances.
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
-    savings_balance = float(input("Enter the savings account balance: "))
-    savings_interest = float(input("Enter the savings account interest rate: "))
-    savings_maturity = int(input("Enter the number of months for the savings account: "))
+    savings_balance = get_float("Enter the savings account balance: ")
+    savings_interest = get_float("Enter the savings account interest rate: ")
+    savings_maturity = get_int("Enter the number of months for the savings account: ")
 
     # Call the create_savings_account function and pass the variables from the user.
-    updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
-
-    # Print out the interest earned and updated savings account balance with interest earned for the given months.
-    print(f"The interest earned on the savings account is: ${interest_earned:.2f}")
-    print(f"The updated savings account balance is: ${updated_savings_balance:.2f}")
-    print(f"This is the updated balance after {savings_maturity} months with an interest rate of {savings_interest}%")
-
+    try:
+        updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
+        # Print out the interest earned and updated savings account balance with interest earned for the given months.
+        print(f"The interest earned on the savings account is: ${interest_earned:.2f}")
+        print(f"The updated savings account balance is: ${updated_savings_balance:.2f}")
+        print(f"This is the updated balance after {savings_maturity} months with an interest rate of {savings_interest}%")
+    except ValueError as e:
+        print(f"An error occured while processingg the savings account: {e}")
+        
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     cd_balance = float(input("Enter the CD account balance: "))
     cd_interest = float(input("Enter the CD account interest rate: "))
